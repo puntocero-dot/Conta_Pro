a6b7d577bb4eea9b160da43fe9736eee087d8ce6
Punto Cero <admin@puntocero.dev>
2026-04-11 20:11:08 -0600

    fix: remove root redirect to enable landing page

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: false,
-      },
-    ];
-  },
+  /* Configuración de la aplicación */
 };
+
 
 export default nextConfig;
